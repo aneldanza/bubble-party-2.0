59a01d957887b1301293516e05168b64eeafa9f7
Anel Danza <aneldanza23@gmail.com>
2025-02-14 16:21:59 -0500

    add navigation and icons

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,4 @@ import "./style.css";
 
 import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="grid grid-cols-1 place-items-center bg-amber-100 h-screen w-screen">
-
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-
-  </div>
-`;
-
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
