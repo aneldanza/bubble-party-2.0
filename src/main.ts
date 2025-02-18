d7279076c8aeed062744a82427ed83141c7f33ab
Anel Danza <aneldanza23@gmail.com>
2025-02-18 12:31:51 -0500

    add colors and find canvas on page load

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+import { COLORS } from "./constants";
 import "./style.css";
+
+// check if page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
+  const colors = COLORS;
+});
