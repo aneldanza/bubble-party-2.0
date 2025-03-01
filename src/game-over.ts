e5bf128ce17ec55f188efe0a7bab213b0779d665
Anel Danza <aneldanza23@gmail.com>
2025-02-28 23:14:53 -0500

    move screen html to index file.

--- src/game-over.ts
+++ src/game-over.ts
@@ -5,57 +5,42 @@ export function setupGameOverScreen(
   game: Game,
   highestScoreManager: HighestScoreManager
 ): void {
-  const gameOverContainer = document.createElement("div");
-  gameOverContainer.id = "game-over";
-  gameOverContainer.classList.add("screen");
-  gameOverContainer.style.display = "none";
-  document.body.appendChild(gameOverContainer);
+  const gameOverElement = document.getElementById("game-over")!;
+  const restartButton = document.getElementById("play-again")!;
+  const goToStartButton = document.getElementById("go-to-start")!;
+  const highestScoreElement = document.getElementById("highest-score")!;
+  const startScreenContainer = document.getElementById(
+    "setup-screen-container"
+  );
 
-  fetch("/views/game-over.html")
-    .then((response) => response.text())
-    .then((html) => {
-      gameOverContainer.innerHTML = html;
+  restartButton.addEventListener("click", () => {
+    gameOverElement.style.display = "none";
+    game.reset();
+    game.start();
+  });
 
-      const gameOverElement = document.getElementById("game-over")!;
-      const restartButton = document.getElementById("play-again")!;
-      const goToStartButton = document.getElementById("go-to-start")!;
-      const highestScoreElement = document.getElementById("highest-score")!;
-      const startScreenContainer = document.getElementById(
-        "setup-screen-container"
-      );
+  game.view.isOver.subscribe(async (value) => {
+    if (value) {
+      const score = game.score.value;
+      const highScore = highestScoreManager.highestScore;
+      if (highScore == null || score > highScore) {
+        await highestScoreManager.setHighScore(score);
+      }
+      gameOverElement.style.display = "flex";
+      highestScoreElement.textContent = highestScoreManager.highestScore
+        ? highestScoreManager.highestScore.toString()
+        : "0";
+    } else {
+      gameOverElement.style.display = "none";
+    }
+  });
 
-      restartButton.addEventListener("click", () => {
-        gameOverElement.style.display = "none";
-        game.reset();
-        game.start();
-      });
-
-      game.view.isOver.subscribe(async (value) => {
-        if (value) {
-          const score = game.score.value;
-          const highScore = highestScoreManager.highestScore;
-          if (highScore == null || score > highScore) {
-            await highestScoreManager.setHighScore(score);
-          }
-          gameOverElement.style.display = "flex";
-          highestScoreElement.textContent = highestScoreManager.highestScore
-            ? highestScoreManager.highestScore.toString()
-            : "0";
-        } else {
-          gameOverElement.style.display = "none";
-        }
-      });
-
-      goToStartButton.addEventListener("click", () => {
-        game.reset();
-        if (startScreenContainer) {
-          startScreenContainer.style.visibility = "visible";
-        } else {
-          // setupStartScreen(game);
-        }
-      });
-    })
-    .catch((error) => {
-      console.error("Error loading game-over.html:", error);
-    });
+  goToStartButton.addEventListener("click", () => {
+    game.reset();
+    if (startScreenContainer) {
+      startScreenContainer.style.visibility = "visible";
+    } else {
+      // setupStartScreen(game);
+    }
+  });
 }
--- src/setup.ts
+++ src/setup.ts
@@ -2,51 +2,42 @@ import Game from "./game";
 import { PlayMode } from "./types";
 
 export function setupStartScreen(game: Game): void {
-  fetch("/views/setup-screen.html")
-    .then((response) => response.text())
-    .then((html) => {
-      const setupScreenContainer = document.createElement("div");
-      setupScreenContainer.id = "setup-screen-container";
-      setupScreenContainer.classList.add("screen");
-      document.body.appendChild(setupScreenContainer);
-      setupScreenContainer.innerHTML = html;
+  const setupScreenContainer = document.getElementById(
+    "setup-screen-container"
+  )!;
 
-      const startButton = document.getElementById("start-game")!;
-      const playModeButtons = document.getElementsByClassName("play-mode");
-      const selectedSVGs = document.getElementsByClassName("mode-svg");
+  const startButton = document.getElementById("start-game")!;
+  const playModeButtons = document.getElementsByClassName("play-mode");
+  const selectedSVGs = document.getElementsByClassName("mode-svg");
 
-      startButton.addEventListener("click", () => {
-        setupScreenContainer.style.visibility = "hidden";
-        game.start();
-      });
+  startButton.addEventListener("click", () => {
+    setupScreenContainer.style.visibility = "hidden";
+    game.start();
+  });
 
-      for (let i = 0; i < playModeButtons.length; i++) {
-        const button = playModeButtons[i];
+  for (let i = 0; i < playModeButtons.length; i++) {
+    const button = playModeButtons[i];
 
-        button.addEventListener("click", () => {
-          game.playMode.value = button.id as PlayMode;
-        });
-      }
-
-      function updateSelectedButton() {
-        for (let i = 0; i < selectedSVGs.length; i++) {
-          const button = selectedSVGs[i] as HTMLElement;
-
-          if (button.getAttribute("data-mode") === game.playMode.value) {
-            button.classList.add("selected");
-          } else {
-            button.classList.remove("selected");
-          }
-        }
-      }
-
-      game.playMode.subscribe(() => {
-        updateSelectedButton();
-      });
-
-      updateSelectedButton();
-    })
-    .catch((error) => {
-      console.error("Error loading setup-screen.html:", error);
+    button.addEventListener("click", () => {
+      game.playMode.value = button.id as PlayMode;
     });
+  }
+
+  function updateSelectedButton() {
+    for (let i = 0; i < selectedSVGs.length; i++) {
+      const button = selectedSVGs[i] as HTMLElement;
+
+      if (button.getAttribute("data-mode") === game.playMode.value) {
+        button.classList.add("selected");
+      } else {
+        button.classList.remove("selected");
+      }
+    }
+  }
+
+  game.playMode.subscribe(() => {
+    updateSelectedButton();
+  });
+
+  updateSelectedButton();
 }
